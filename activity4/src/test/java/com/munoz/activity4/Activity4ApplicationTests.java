390589e8eef6d6d7cbd402c8507efe59e532abb8
Mara Munoz <maukamnm@gmail.com>
2022-04-27 22:47:34 -0700

    local application.prop

---
+++ activity4/src/test/java/com/munoz/activity4/Activity4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.munoz.activity4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Activity4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
